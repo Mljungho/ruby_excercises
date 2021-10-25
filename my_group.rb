e5190c0f59fa3cb7d63bfaa6287c1a9a5bb87d1b
Mljungho <mathias.ljungholm@gmail.com>
2021-10-25 20:45:25 +0200

    Managed to get hashes to work, need to fix arrays

--- my_group.rb
+++ my_group.rb
@@ -7,4 +7,4 @@ person_2 = {name: "Elizabeth", sex: "Woman", age: 35}
 
 person_3 = {name: "Paul", sex: "Man", age: 44}
 
-puts "my name is #{person_1[:name]} and I am #{person_1[:age]} old. #{person_2[:name]} is #{person_2[:age]}"
+puts "my name is #{my_group[:name]} and I am #{person_1[:age]} old. #{person_2[:name]} is #{person_2[:age]}"
