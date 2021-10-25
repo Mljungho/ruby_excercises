ed9020918a269becb1ed4f1168c0875bf064fcdb
Mljungho <mathias.ljungholm@gmail.com>
2021-10-25 15:55:11 +0200

    Hashes & Array defined

---
+++ my_group.rb
@@ -0,0 +1,20 @@
+
+person_1 = {
+    "name"  => "David"
+    "sex"   => "Male"
+    "age"   => "21"
+}
+person_2 = {
+    "name"  => "Elizabeth"
+    "sex"   => "Female"
+    "age"   => "35"
+}
+person_3 = {
+    "name"  => "Paul"
+    "sex"   => "Male"
+    "age"   => "44"
+}
+
+
+    my_group = Array ["person_1", "person_2", "person_3"]
+put my_group [0]
