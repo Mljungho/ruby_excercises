cd76ed411c06fa13130645d44e33fd98477c2596
Mljungho <mathias.ljungholm@gmail.com>
2021-10-25 23:05:11 +0200

    Finallized exercise with materemonial intervention.

--- my_group.rb
+++ my_group.rb
@@ -1,15 +1,17 @@
 
-my_group = Array ["person_1", "person_2", "person_3"]
-
 person_1 = {name: "David", sex: "Man", age: 21}
 
 person_2 = {name: "Elizabeth", sex: "Woman", age: 35}
 
 person_3 = {name: "Paul", sex: "Man", age: 44}
 
+my_group = Array [person_1, person_2, person_3]
+
+my_group
+
 puts(my_group[0])
 puts "my name is #{person_1[:name]} and I am #{person_1[:age]} old. #{person_2[:name]} is #{person_2[:age]}"
 
-my_group.each do |my_group|
-    puts "Hello #{my_group}"
+my_group.each do |person|
+    print "my name is ", person[:name], ". ", "I am ", person[:age], " years old ", "and I am a ", person[:sex], "\n" # "\n", command creates a brek for every new 'array line'.
 end
