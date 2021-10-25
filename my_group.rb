22982ba6f374c4caddbbc45a63c6df2073f07310
Mljungho <mathias.ljungholm@gmail.com>
2021-10-25 16:13:32 +0200

    fixed Syntrax Error

--- my_group.rb
+++ my_group.rb
@@ -1,20 +1,25 @@
 
 person_1 = {
-    "name"  => "David"
-    "sex"   => "Male"
-    "age"   => "21"
+    "name"  => "David",
+    "sex"   => "Male",
+    "age"   => "21",
+
 }
+
 person_2 = {
-    "name"  => "Elizabeth"
-    "sex"   => "Female"
-    "age"   => "35"
+    "name"  => "Elizabeth",
+    "sex"   => "Female",
+    "age"   => "35",
+
 }
+
 person_3 = {
-    "name"  => "Paul"
-    "sex"   => "Male"
-    "age"   => "44"
+    "name"  => "Paul",
+    "sex"   => "Male",
+    "age"   => "44",
+
 }
 
 
     my_group = Array ["person_1", "person_2", "person_3"]
-put my_group [0]
+puts my_group [0]
