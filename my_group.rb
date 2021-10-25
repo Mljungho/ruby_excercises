e7da299d65189c3b82d06e4dde980c96e985258d
Mljungho <mathias.ljungholm@gmail.com>
2021-10-25 21:58:53 +0200

    want to save to show mistake

--- my_group.rb
+++ my_group.rb
@@ -1,5 +1,5 @@
 
-   my_group = Array ["David", "Elizabeth", "Bacon"]
+my_group = Array ["person_1", "person_2", "person_3"]
 
 person_1 = {name: "David", sex: "Man", age: 21}
 
@@ -7,4 +7,9 @@ person_2 = {name: "Elizabeth", sex: "Woman", age: 35}
 
 person_3 = {name: "Paul", sex: "Man", age: 44}
 
-puts "my name is #{my_group[:name]} and I am #{person_1[:age]} old. #{person_2[:name]} is #{person_2[:age]}"
+puts(my_group[0])
+puts "my name is #{person_1[:name]} and I am #{person_1[:age]} old. #{person_2[:name]} is #{person_2[:age]}"
+
+my_group.each do |my_group|
+    puts "Hello #{my_group}"
+end
